feec85fdaa30f5396efb51ea495c8685021059c0
Lucasuha <lucas.heymann@uha.fr>
2024-10-15 16:34:04 +0200

    Add files via upload

---
+++ TP1exo1.py
@@ -0,0 +1,9 @@
+nom = "Heymann"
+prenom = "Lucas"
+math = 14
+anglais = 17
+info = 16
+promotion = 2024
+moy =((math) + (anglais) + (info))/3
+print(type(nom),type(prenom),type(math),type(anglais),type(info),type(promotion),type(moy))
+print("L'etudiant {} {} de la promotion {} a une moyenne de {}" .format(nom, prenom, promotion, moy))
---
+++ TP1exo2.py
@@ -0,0 +1,5 @@
+jour = 15
+heure = 15
+minute = 48
+Nbmin = (1440*(jour-1)) + (60 *(heure)) + minute
+print(Nbmin)
